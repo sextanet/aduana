6ccdb9ddd1504cd62c98a6e08314121f29e09c0d
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2025-01-02 17:20:43 -0300

    feat: add functions

---
+++ tests/HelpersTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use SextaNet\Aduana\Aduana;
+
+it('can set a password', function () {
+    aduana_password('1234567890');
+    
+    expect(Aduana::hasPassword())
+        ->toBeTrue();
+});
+
+it('can encrypt', function () {
+    aduana_password('1234567890');
+    $encrypted = aduana_encrypt('encrypted text');
+    
+    expect($encrypted)
+        ->not->toBe('encrypted text');
+});
+
+it('can decrypt', function () {
+    aduana_password('1234567890');
+    $encrypted = aduana_encrypt('encrypted text');
+    $decrypted = aduana_decrypt($encrypted);
+    
+    expect($decrypted)
+        ->toBe('encrypted text');
+});
--- tests/MagicTest.php
+++ tests/MagicTest.php
@@ -10,3 +10,13 @@ beforeEach(function () {
 it('the password needs to be at least 10 characters', function () {
     Aduana::setPassword('123456789');
 })->expectException(TooShortPassword::class);
+
+it('knows when it has a password', function () {
+    expect(Aduana::hasPassword())
+        ->toBeFalse();
+
+    Aduana::setPassword('123456789');
+
+    expect(Aduana::hasPassword())
+        ->toBeTrue();
+})->expectException(TooShortPassword::class);
